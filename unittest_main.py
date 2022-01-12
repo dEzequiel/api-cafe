018c5afc64011633699aebb3650a041064bba08d
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-12 18:35:43 +0100

    test(test_home): add test to evaluate status code

--- unittest_main.py
+++ unittest_main.py
@@ -2,7 +2,14 @@ import unittest
 from main import *
 
 class APICafeTest(unittest.TestCase):
+    
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        
+        self.assertEqual(response.status_code, 200)
+    
+    
 
-    def test_get_random_cafe(self):
-        value = get_random_cafe()
-        self.assertIsInstance(get_random_cafe(), dict)
+if __name__ == '__main__':
+    unittest.main()
